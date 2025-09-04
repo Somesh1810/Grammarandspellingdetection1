7602ed9ac35b7572718d2bee48fef20479c3e364
someshwar <msomeshn@gmail.com>
2025-09-04 11:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from textblob import TextBlob
 
 st.title("Grammar and Spelling Checker (TextBlob)")
 
-input_text = st.text_area("Enter your text:", height=200)
+st.markdown("### Enter your text:")
+input_text = st.text_area("", height=200)
+
+corrected_text = ""
 
 if st.button("Correct Grammar"):
     if input_text.strip() == "":
         st.warning("Please enter some text to correct.")
     else:
         blob = TextBlob(input_text)
-        corrected = blob.correct()
+        corrected_text = blob.correct()
         st.success("✅ Grammar and spelling corrected.")
-        st.text_area("Corrected Text:", value=str(corrected), height=200)
+
+if corrected_text:
+    st.markdown("### Corrected Text:")
+    corrected_text = st.text_area("", value=str(corrected_text), height=200)
